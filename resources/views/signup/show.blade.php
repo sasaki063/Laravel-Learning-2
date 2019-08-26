00ddd7da60589ea2176a3432056ac70aef9f4e23
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-08-26 19:25:53 +0900

    develop

---
+++ resources/views/signup/show.blade.php
@@ -0,0 +1,12 @@
+<header>
+  <a href="/">TOP</a>
+  <a href="/signup">Signup</a>
+</header>
+
+<form action="/signup" method="post">
+  <p>Name: <input type="text" name="name" size="40"></p>
+  <p>E-mail: <input type="text" name="name" size="40"></p>
+  <p>Password: <input type="password" name="name" size="40"></p>
+  <p>ConfirmPassword: <input type="password" name="name" size="40"></p>
+  <p><input type="submit" value="送信"></p>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HelloController@index');
+Route::get('signup', 'HelloController@show');
